65bb0b3277f79e81d9af56ef68d8cc9f88fca3aa
K MOHITH KANNAN <promohith.535@email.com>
2026-02-17 13:43:10 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPS Banner App - UC1
+ *
+ * Prints literal "OOPS" to console
+ *
+ * @author Mohith
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
