e28da97f36d2174a5de17179518ac5c4088d893f
K MOHITH KANNAN <promohith.535@email.com>
2026-02-24 14:58:46 +0530

    UC6: Use static helper functions for O, P, S banner patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,68 @@
 /**
- * OOPS Banner App - UC5
+ * OOPS Banner App - UC6
  *
- * Displays OOPS banner using array initialization with String.join()
+ * Uses static helper methods to generate character patterns
  *
  * @author Mohith
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("", " *****", "   *****", "   *****", "   ***** "),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", " *****", "   *****", "   *****", "   ***** ")
-        };
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
+        // Build final OOPS banner line by line
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
+
+        // Print banner
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Static method for O
+    static String[] getO() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Static method for P
+    static String[] getP() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Static method for S
+    static String[] getS() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
